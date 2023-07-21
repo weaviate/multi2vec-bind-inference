a504d2496190da7bd3559a5c1aa4789cf7704bc0
Marcin Antas <antas.marcin@gmail.com>
2023-07-21 16:17:46 +0200

    Add dynamic info,json support for /meta endpoint

--- download.py
+++ download.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 from ImageBind.models import imagebind_model
+import json
 
 print("Downloading ImageBind model...")
 
 # This command will download model to .checkpoints directory
-imagebind_model.imagebind_huge(pretrained=True)
+model = imagebind_model.imagebind_huge(pretrained=True)
+
+# Create an info.json file with model basic information
+info = {
+  "model": model._get_name(),
+  "version": model._version,
+}
+with open("./.checkpoints/info.json", 'w+') as json_file:
+  json.dump(info, json_file, indent = 2)
 
 print("Success")
